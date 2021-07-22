23a0da5cbe8e204dc0874d60da56201513815a8c
Franciane Lark <franciane.lark019@gmail.com>
2021-07-21 22:52:13 -0400

    operadores matematicos

--- Javascript/exemplo4.js
+++ Javascript/exemplo4.js
@@ -4,4 +4,28 @@ result = n1 + n2
 
 console.log("Soma = ", result)
 
+result = n1 - n2
+
+console.log("Subtracao = ", result)
+
+result = n1 * n2
+
+console.log("Multiplicacao = ", result)
+
+result = n1 / n2
+
+console.log("Divisao = ", result)
+
+console.log(n1++)
+
+console.log(n1)
+
+console.log(++n1)
+
+result = n1 % n2
+
+console.log("Resto da divisao", result)
+
+
+
 
