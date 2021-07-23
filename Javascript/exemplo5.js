f20487454ae8d449255c0c01071aef8f1bd1340e
Franciane Lark <franciane.lark019@gmail.com>
2021-07-22 22:19:56 -0400

    Estrutura de condição

---
+++ Javascript/exemplo5.js
@@ -0,0 +1,21 @@
+let idade = 18
+
+if ( idade >=18){
+  console.log("Maior de idade:")
+}else{
+  console.log("Menor de idade:")
+}
+
+let sexo = 'M'
+
+switch(sexo){
+  case 'M':
+    console.log("Masculino")
+    break;
+
+  case 'F':
+    console.log("Feminino")
+    break:
+
+}
+
