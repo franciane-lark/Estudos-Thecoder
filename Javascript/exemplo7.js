2be7439076d0e90f400156b2a3896a1c64430b06
Franciane Lark <franciane.lark019@gmail.com>
2021-07-24 23:26:28 -0400

    Conversao de tipos

---
+++ Javascript/exemplo7.js
@@ -0,0 +1,12 @@
+console.log ( Number ("3"))// 3
+console.log ( Number("3.7237"))// 3.7237
+console.log ( Number("Franciane"))// NaN (Not a Number)
+console.log ( Number(false))// 0
+console.log ( Number(true))// 1
+console.log ( parseInt("90"))// 90
+console.log ( parseInt("90.29393"))// 90
+console.log ( parseInt("Lark"))// NaN
+console.log ( parseFloat("4"))// 4
+console.log ( parseFloat("4.7841"))// 4.7841
+console.log ( parseFloat("Silva"))// NaN
+console.log ( parseInt("10") + parseInt("7"))
