3d7165a329a33eefc44f14a00d537f9ded87dbb0
Franciane Lark <franciane.lark019@gmail.com>
2021-07-27 14:51:15 -0400

    atualizacao aulas

---
+++ Javascript/exemplo10.js
@@ -0,0 +1,22 @@
+//let, var
+
+let num = 10;
+console.log("Num: ", num1)
+
+if(true){
+  let num1 = 50
+  console.log("Num: ", num1)
+}
+console.log("Valor de Num: ", num1)
+
+if(true){
+  var novoNumero = 180
+  console.log("Novo numero = ", novoNumero)
+}
+console.log("Novo numero = ", novoNumero)
+
+if(true){
+  var TesteNumero = 150
+  console.log("Novo numero = ", testeNumero)
+}
+console.log("Novo numero = ", testeNumero)
---
+++ Javascript/exemplo9.js
@@ -0,0 +1,20 @@
+for (let num = 10; num < 50; num++){
+  console.log("Numero = ", num)
+}
+//Inicilizar variavel de condicao
+let valor = 0;
+while( valor <= 10){//
+  console.log("Valor com WHILE = ", valor)
+  valor++;
+}
+//Incremento da condição
+/*
+No caso da utilização do DO.. WHILE o programa vai executar o bloco de comando pleo menos 1 vez
+*/
+
+let num2 = 150
+do{
+  console.log("DO ... WHILE ", num2)
+  num2++
+  //incremento da condição
+}while(num2 < 15);//condição para o while executar
