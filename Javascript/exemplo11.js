fd17a2e253453e674e53da224ba193b11c7d626b
Franciane Lark <franciane.lark019@gmail.com>
2021-08-02 23:16:19 -0400

    valor randomino

---
+++ Javascript/exemplo11.js
@@ -0,0 +1,13 @@
+//pegar um valor randomino
+console.log(Math.random())
+
+console.log(Math.random() *10)
+
+console.log(Math.random() *100)
+
+console.log((Math.random() *10) + 1)
+
+console.log(Math.round(8.42))
+
+console.log(Math.round(8.72))
+
